2b535a64ce36268845ecb2e8f20e2ceb9c7287ca
gilbertsirkin <gilbertsirkin@gmail.com>
2026-08-04 10:07:39 +0100

    Simplify WSGI application import
    Refactor application callable to simplify import.

--- api/index.py
+++ api/index.py
@@ -1,14 +1,1 @@
-from urllib.parse import parse_qs
-from wolvcapital.wsgi import application as _application
-
-def application(environ, start_response):
-    # If Vercel provided the original path via __orig_path query param, use it
-    qs = environ.get("QUERY_STRING", "")
-    params = parse_qs(qs)
-    orig = params.get("__orig_path")
-    if orig:
-        environ["PATH_INFO"] = orig[0]
-    return _application(environ, start_response)
-
-# Expose the WSGI callable Vercel expects
-app = application
+from wolvcapital.wsgi import application as app
